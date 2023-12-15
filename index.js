0714225635ce7b24d63de25c275d9a0d2dbf9f00
immanuelcherui1 <mmanuelcherui1@gmail.com>
2023-12-15 19:09:52 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const companyName= "Scuber"; // Code your solution in this file!
+const companyName= 'Scuber';
 
+let mostProfitableNeighborhood='Chelsea';
 
-let mostProfitableNeighborhood = 'Chelsea'
-
-
-let companyCeo = 'Susan Smith'
+let companyCeo='Susan Smith';
